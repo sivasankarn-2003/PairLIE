10ba997df901652bd7a4fa93b50f6b9c3c27a211
G.Sivasankaran <145788824+sivasankarn-2003@users.noreply.github.com>
2024-11-09 23:30:36 +0530

    eval.py

--- eval.py
+++ eval.py
@@ -1,78 +1,58 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
-import time
+import torch
 import argparse
-from thop import profile
-from net.net import net
-from data import get_eval_set
 from torchvision import transforms
 from torch.utils.data import DataLoader
+from PIL import Image
+from net.net import net
 from utils import *
 
+# Set GPU device if applicable
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
 parser = argparse.ArgumentParser(description='PairLIE')
-parser.add_argument('--testBatchSize', type=int, default=1, help='testing batch size')
+parser.add_argument('--input_image_path', type=str, required=True, help='Path to the input image')
 parser.add_argument('--gpu_mode', type=bool, default=True)
-parser.add_argument('--threads', type=int, default=0, help='number of threads for data loader to use')
-parser.add_argument('--rgb_range', type=int, default=1, help='maximum value of RGB')
-# parser.add_argument('--data_test', type=str, default='../dataset/LIE/LOL-test/raw')
-# parser.add_argument('--data_test', type=str, default='../dataset/LIE/SICE-test/image')
-parser.add_argument('--data_test', type=str, default='../dataset/LIE/MEF')
 parser.add_argument('--model', default='weights/PairLIE.pth', help='Pretrained base model')  
-parser.add_argument('--output_folder', type=str, default='results/MEF/')
+parser.add_argument('--output_folder', type=str, default='results/')
 opt = parser.parse_args()
 
+# Ensure output folder exists
+os.makedirs(opt.output_folder, exist_ok=True)
 
-print('===> Loading datasets')
-test_set = get_eval_set(opt.data_test)
-testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=1, shuffle=False)
+print('===> Loading image')
+def load_image(image_path):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    image = Image.open(image_path).convert('RGB')
+    image = transform(image).unsqueeze(0)  # Add batch dimension
+    return image, os.path.basename(image_path)
+
+input_image, image_name = load_image(opt.input_image_path)
+print(f'Loaded image: {image_name}')
 
 print('===> Building model')
-model = net().cuda()
-model.load_state_dict(torch.load(opt.model, map_location=lambda storage, loc: storage))
+model = net().cuda() if opt.gpu_mode else net()
+model.load_state_dict(torch.load(opt.model, map_location='cuda' if opt.gpu_mode else 'cpu'))
 print('Pre-trained model is loaded.')
 
 def eval():
     torch.set_grad_enabled(False)
     model.eval()
     print('\nEvaluation:')
-    for batch in testing_data_loader:
-        with torch.no_grad():
-            input, name = batch[0], batch[1]
-        input = input.cuda()
-        print(name)
+    
+    input_image_tensor = input_image.cuda() if opt.gpu_mode else input_image
+    with torch.no_grad():
+        L, R, X = model(input_image_tensor)
+        D = input_image_tensor - X
+        I = torch.pow(L, 0.2) * R  # Adjust the exponent as needed (e.g., 0.2, LOL=0.14)
+        
+        # Save the processed image
+        output_path = os.path.join(opt.output_folder, f'output_{image_name}')
+        save_image(I, output_path)  # Implement this utility function if not present
+        
+        print(f'Output saved to {output_path}')
 
-        with torch.no_grad():
-            L, R, X = model(input)
-            D = input- X        
-            I = torch.pow(L,0.2) * R  # default=0.2, LOL=0.14.
-            # flops, params = profile(model, (input,))
-            # print('flops: ', flops, 'params: ', params)
-
-        if not os.path.exists(opt.output_folder):
-            os.mkdir(opt.output_folder)
-            os.mkdir(opt.output_folder + 'L/')
-            os.mkdir(opt.output_folder + 'R/')
-            os.mkdir(opt.output_folder + 'I/')  
-            os.mkdir(opt.output_folder + 'D/')                       
-
-        L = L.cpu()
-        R = R.cpu()
-        I = I.cpu()
-        D = D.cpu()        
-
-        L_img = transforms.ToPILImage()(L.squeeze(0))
-        R_img = transforms.ToPILImage()(R.squeeze(0))
-        I_img = transforms.ToPILImage()(I.squeeze(0))                
-        D_img = transforms.ToPILImage()(D.squeeze(0))  
-
-        L_img.save(opt.output_folder + '/L/' + name[0])
-        R_img.save(opt.output_folder + '/R/' + name[0])
-        I_img.save(opt.output_folder + '/I/' + name[0])  
-        D_img.save(opt.output_folder + '/D/' + name[0])                       
-
-    torch.set_grad_enabled(True)
-
-eval()
-
-
+if __name__ == "__main__":
+    eval()
